e8e11218759086b4f828c1fdf8987ba2e9718aad
rhythmic-awareness <simmons.benjamin@gmail.com>
2026-04-24 15:43:47 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-﻿import React from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import RhythmicAwarenessLandingPage from "./App.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RhythmicAwarenessLandingPage />
+    <Analytics />
   </React.StrictMode>
 );
